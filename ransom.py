74411e7407d43c233065843206aeccc9bf51d969
chautuanminh <tuanminhchau31@gmail.com>
2025-07-03 00:20:46 +0700

    late night coding

---
+++ ransom.py
@@ -0,0 +1,22 @@
+vfrom typing import *
+from collections import defaultdict
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter = defaultdict(int)
+        for c in magazine:
+            counter[c] += 1
+        for z in ransomNote:
+            if z not in counter:
+                return False
+            else: 
+                counter[z] -= 1
+                if counter[z] == 0:
+                    counter.pop(z)
+        return True                
+def main():
+    sol = Solution()
+    ran = "hell"
+    maga = "hello"
+    result = sol.canConstruct(ran, maga)
+    print(result)
+main()
